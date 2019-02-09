463544164d292071b1a9d9bcbb517034b9ace153
heykishan <bkishan98@gmail.com>
2019-02-09 22:47:17 +0530

    major updates

---
+++ analyze.py
@@ -0,0 +1,6 @@
+import csv
+import sys
+import os
+
+def begin_analyze():
+    os.startfile("test_results.csv")
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import test
+from analyze import begin_analyze
 import csv
 from tkinter import *
 
@@ -18,13 +18,16 @@ def begin_test():
     cinfo_two = component_two.get()
     cinfo_three = component_three.get()
     cinfo_four = component_four.get()
+    cinfo_five = component_five.get()
 
-    myData = [[cinfo_one], [cinfo_two], [cinfo_three], [cinfo_four]] 
+    myData = [[cinfo_one], [cinfo_two], [cinfo_three], [cinfo_four], [cinfo_five]] 
     myFile = open('test.csv', 'w')  
     with myFile:
         writer = csv.writer(myFile)
         writer.writerows(myData)
         print("written successfully")
+    
+    begin_analyze()
 
     
 component_one = Label(text = "Componenet 1")
@@ -39,20 +42,26 @@ component_three.place(x = 2, y = 310)
 component_four = Label(text = "Componenet 4")
 component_four.place(x = 2, y = 390)
 
+component_five = Label(text = "Componenet 5")
+component_five.place(x = 2, y = 470)
+
 component_one = StringVar()
 component_two = StringVar()
 component_three = StringVar()
 component_four = StringVar()
+component_five = StringVar()
 
 cp1_entry = Entry(textvariable = component_one, width = 30)
 cp2_entry = Entry(textvariable = component_two, width = 30)
 cp3_entry = Entry(textvariable = component_three, width = 30)
 cp4_entry = Entry(textvariable = component_four, width = 30)
+cp5_entry = Entry(textvariable = component_five, width = 30)
 
 cp1_entry.place( x= 150, y = 150)
 cp2_entry.place( x= 150, y = 230)
 cp3_entry.place( x= 150, y = 310)
 cp4_entry.place( x= 150, y = 390)
+cp5_entry.place( x= 150, y = 470)
 
 register = Button(screen,text = "Analyze", width = "30", height = "3", command = begin_test, bg = "white")
 register.place(x = 500, y = 600)
